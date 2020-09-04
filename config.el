85dede2a4360b407c15f563f60c806be70aa03ce
Alex WANG <wangtcalex@gmail.com>
2020-09-04 21:08:19 +0800

    update

---
+++ config.el
@@ -0,0 +1,99 @@
+;; Private packagies
+(add-to-list 'load-path "~/.doom.d/elisp")
+;; (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+(setq user-full-name "WANG Tianchang Alex"
+      user-mail-address "wangtcalex@gmail.com")
+
+;; (setq doom-font (font-spec :family "Source Code Variable" :size 18))
+(setq doom-font (font-spec :family "Fira Code" :size 16))
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+  (set-fontset-font (frame-parameter nil 'font)
+                    charset (font-spec :family "PingFang SC"
+                                       :size 16)))
+(setq-default line-spacing 2)
+
+
+
+(defun alex/disable-bold-and-fringe-bg-face-globally ()
+"Disable bold face and fringe background in Emacs."
+(interactive)
+(set-face-attribute 'fringe nil :background nil)
+(mapc #'(lambda (face)
+            (when (eq (face-attribute face :weight) 'bold)
+            (set-face-attribute face nil :weight 'normal)))
+      (face-list)))
+
+(add-hook 'after-init-hook #'alex/disable-bold-and-fringe-bg-face-globally)
+
+
+;;(setq doom-theme 'doom-one)
+;; (setq doom-theme 'doom-acario-dark)
+(setq doom-theme 'doom-one)
+
+(setq org-directory "~/org/")
+
+(setq display-line-numbers-type t)
+
+(setq mac-option-modifier 'meta)
+(setq mac-command-modifier 'super)
+(global-set-key (kbd "s-c") #'clipboard-kill-ring-save)
+(global-set-key (kbd "s-v") #'clipboard-yank)
+(global-set-key (kbd "s-w") #'delete-frame)
+(global-set-key (kbd "s-o") #'ace-window)
+(global-set-key (kbd "s-x") #'execute-extended-command)
+(global-set-key (kbd "M-x") #'execute-extended-command)
+(global-set-key (kbd "s-n") #'make-frame-command)
+(global-set-key (kbd "s-s") #'save-buffer)
+
+
+;; Some personaliized Evil settings
+;;
+;; Vim style buffer management
+;; Reserve the operate style of original Emacs
+(with-eval-after-load 'evil-maps
+(define-key evil-insert-state-map (kbd "C-n") nil)
+(define-key evil-insert-state-map (kbd "C-f") nil)
+(define-key evil-insert-state-map (kbd "C-b") nil)
+(define-key evil-insert-state-map (kbd "C-p") nil))
+(evil-set-initial-state 'term-mode 'emacs)
+(defun alex/save-and-kill-this-buffer ()
+    (interactive)
+    (save-buffer)
+    (kill-this-buffer))
+(evil-ex-define-cmd "q" 'kill-this-buffer)
+(evil-ex-define-cmd "wq" 'alex/save-and-kill-this-buffer)
+
+
+;; Package Preference
+
+;; More completion congig
+(setq company-idle-delay 0.01)
+;; (set-company-backend! 'prog-mode
+;;   'company-tabnine 'company-capf 'company-yasnippet)
+(setq org-latex-pdf-process
+      '("xelatex -interaction nonstopmode -output-directory %o %f"
+        "xelatex -interaction nonstopmode -output-directory %o %f"
+        "xelatex -interaction nonstopmode -output-directory %o %f"))
+
+;; (use-package! telega
+;;   :load-path  "~/telega.el"
+;;   :commands (telega)
+;;   :defer t)
+;;
+
+(use-package! tron-legacy-theme
+  :config
+  (setq tron-legacy-theme-vivid-cursor t)
+  (load-theme 'tron-legacy t))
+
+(use-package! org-bullets
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+  )
+
+(use-package! auto-save
+  :config
+  (auto-save-enable)
+  (setq auto-save-silent t)
+  (setq auto-save-delete-trailing-whitespace nil))
---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" "95c5418fab44795d0a323482bc3d8fe1ecf0c32a6775c59c028c8844e29b5ed3" "f2927d7d87e8207fa9a0a003c0f222d45c948845de162c885bf6ad2a255babfd" "c4bdbbd52c8e07112d1bfd00fee22bf0f25e727e95623ecb20c4fa098b74c1bd" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
